6f0c28c1b04a2eb1b7a4b39a9db071b6f12e348f
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2016-06-18 11:21:26 +0200

    Change the implementation from `O(n * n)` to `O(n log n)`

--- inversions.hs
+++ inversions.hs
@@ -11,21 +11,21 @@ countInversions arr = let mid   = (length arr) `div` 2
                           right = drop mid arr
                           (left'  , l) = countInversions $! left
                           (right' , r) = countInversions $! right
-                          (whole  , w) = id $! countSplit left' right'
+                          (whole  , w) = id $! countSplit mid left' right'
                       in (whole, l + r + w)
 
-countSplit :: [Int] -> [Int] -> ([Int], Int)
-countSplit [] y = (y, 0)
-countSplit x [] = (x, 0)
-countSplit (x:xs) (y:ys)
+countSplit :: Int -> [Int] -> [Int] -> ([Int], Int)
+countSplit _ [] y = (y, 0)
+countSplit _ x [] = (x, 0)
+countSplit len (x:xs) (y:ys)
         | x <= y    = ((x: left ), 0 + l)
         | otherwise = ((y: right), 0 + r)
-            where leftSplit  = countSplit xs (y:ys)
-                  rightSplit = countSplit (x:xs) ys
+            where leftSplit  = countSplit (len - 1) xs (y:ys)
+                  rightSplit = countSplit len (x:xs) ys
                   left  = fst leftSplit
                   right = fst rightSplit
                   l     = snd leftSplit
-                  r     = (snd rightSplit) + length((x: xs))
+                  r     = (snd rightSplit) + len
 
 main = do
     nums <- liftM (map (read :: String -> Int) . lines) getContents
